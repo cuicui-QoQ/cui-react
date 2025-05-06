29d5b1a081b4905a4e69b7b5288b3384c2e10238
cuicui <wyg.scut@qq.com>
2025-05-06 15:45:02 +0800

    处理部分属性

--- src/react.dom.js
+++ src/react.dom.js
@@ -36,8 +36,25 @@ function createDOM(VNode) {
             dom.appendChild(document.createTextNode(props.children))
         }
     }
-    // todo 这里的属性没有进行处理和绑定
+    setPropsForDOM(dom, props)
     return dom;
+}
+
+function setPropsForDOM(dom, VNodeProps = {}) {
+    if(!dom) return
+    for (let key in VNodeProps) {
+        if (key === 'children') continue;
+        // 匹配事件
+        if (/^on[A-Z].*/.test(key)) {
+            // 后续对事件做处理
+        } else if (key === 'style') {
+            Object.keys(VNodeProps[key]).forEach(styleName => {
+                dom.style[styleName] = VNodeProps[key][styleName]
+            })
+        } else {
+            dom[key] = VNodeProps[key]
+        }
+    }
 }
 
 function mountArray(children, parent) {
